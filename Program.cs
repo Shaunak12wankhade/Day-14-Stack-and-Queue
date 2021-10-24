7ce0ba96c383e6731c4a5e3d38b3da61e9451da0
unknown <shaunak12wankhade@ggmail.com>
2021-10-24 15:13:07 +0530

    [Shaunak] Added : create a stack

--- Program.cs
+++ Program.cs
@@ -10,7 +10,14 @@ namespace stackandqueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to stack and queue");
+            //Console.WriteLine("Welcome to stack and queue");
+
+            Console.WriteLine("Stack & Queue");
+            StackQueue<int> stackListNode = new StackQueue<int>();
+            stackListNode.Push(70);
+            stackListNode.Push(30);
+            stackListNode.Push(56);
+            stackListNode.Show();
         }
     }
 }
---
+++ StackQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackandqueue
+{
+   
+        class StackQueue<Gtype>
+        {
+            private Node<Gtype> top;
+            public StackQueue()
+            {
+                this.top = null;
+
+            }
+            public void Push(int d)
+            {
+                Node<Gtype> node = new Node<Gtype>(d);
+                if (this.top == null)
+                    node.Next = null;
+                else
+                    node.Next = this.top;
+                this.top = node;
+                Console.WriteLine($"data : {d}");
+            }
+            public void Show()
+            {
+                Node<Gtype> temp = this.top;
+                while (temp != null)
+                {
+                    Console.WriteLine($"{temp.Data} ");
+                    temp = temp.Next;
+                }
+            }
+        }
+}
